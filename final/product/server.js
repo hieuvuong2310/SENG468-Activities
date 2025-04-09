7c565a06d4124e9c74152e795ae4d1a1b670f96b
hieuvuong2310 <hieuvuong2310@gmail.com>
2025-04-09 10:08:37 -0700

    Part B3 done

--- final/product/server.js
+++ final/product/server.js
@@ -1,18 +1,32 @@
 const express = require("express");
+const Redis = require("ioredis");
 const app = express();
-app.use(express.json());
-
+const redis = new Redis({ host: "redis", port: 6379 });
 const PORT = 3002;
 
-app.get("/check", (req, res) => {
-  const productId = req.query.productId;
-  const available = productId === "1"; // simulate one available product
-  console.log(
-    `[${new Date().toISOString()}] 🔍 Inventory check for ${productId}: ${available}`
-  );
-  res.json({ available });
+const products = {
+  1: { name: "Keyboard", category: "electronics" },
+  2: { name: "Shampoo", category: "personal_care" },
+};
+
+app.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const cacheKey = `product:${id}`;
+  const cached = await redis.get(cacheKey);
+
+  if (cached) {
+    console.log(`CACHE HIT for ${cacheKey}`);
+    return res.json(JSON.parse(cached));
+  }
+
+  const product = products[id];
+  if (!product) return res.status(404).send("Not found");
+
+  console.log(`CACHE MISS for ${cacheKey}`);
+  await redis.set(cacheKey, JSON.stringify(product), "EX", 3600); // TTL = 60s
+
+  res.json(product);
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 [ProductService] running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log("Product service on port 3002"));
